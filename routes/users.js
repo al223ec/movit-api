20abe7e15fc9269f9da082c5342dcf7e0f0c5fa5
Anton Ledström <anton.ledstrom@digitalent.se>
2019-03-24 22:41:32 +0100

    added check for correct user

--- routes/users.js
+++ routes/users.js
@@ -20,13 +20,19 @@ const generateHash = password => new Promise((resolve, reject) => {
 
 module.exports = {
   show: async (req, res, next) => {
-    try {
-      const user = await User.findById(req.params.id);
-      res.send(user);
-      next();
-    } catch (err) {
-      return next(new errors.ResourceNotFoundError(`There is no customer with the id of ${req.params.id}`));
+    const { id } = req.params;
+    const { user: { _id: currentUserId } } = req;
+
+    if (id !== currentUserId) {
+      return next(new errors.UnauthorizedError('Invalid user id provided'));
     }
+
+    return User.findById(req.params.id)
+      .then((user) => {
+        res.send(user);
+        return next();
+      })
+      .catch(() => next(new errors.ResourceNotFoundError(`There is no customer with the id of ${id}`)));
   },
   // Register User
   create: (req, res, next) => {
