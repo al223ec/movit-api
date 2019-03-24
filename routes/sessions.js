77b9dbb998cd7c884ce6c0f5ee0d9e41366d4d09
Anton Ledström <anton.ledstrom@digitalent.se>
2019-03-24 21:41:49 +0100

    can recieve jwt

---
+++ routes/sessions.js
@@ -0,0 +1,29 @@
+const errors = require('restify-errors');
+const jwt = require('jsonwebtoken');
+const auth = require('../auth');
+const config = require('../config');
+
+module.exports = {
+  create: async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+      // Authenticate User
+      const user = await auth.authenticate(email, password);
+
+      // Create JWT
+      const token = jwt.sign(user.toJSON(), config.JWT_SECRET, {
+        expiresIn: '15m'
+      });
+
+      const { iat, exp } = jwt.decode(token);
+      // Respond with token
+      res.send({ iat, exp, token });
+
+      next();
+    } catch (err) {
+      // User unauthorized
+      return next(new errors.UnauthorizedError(err));
+    }
+  },
+};
